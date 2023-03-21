632cb93928c9e1b007c86154196995aadeb4c11b
Sakhi <sakhybig@gmail.com>
2023-03-21 20:31:41 +0200

    edited the index page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  const[results,setResults]=useState([]);
+
+  React.useEffect(()=>{
+    fetch("http://localhost:3001/categories")
+    .then(response=>response.json())
+    .then(data=>{console.log(data);
+      setResults(data);})
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>Mini Store</header>
+
+    <section>
+    <nav>
+      {results.map(d => (
+        <div key={d.id}>{d.title}</div>
+      ))}
+    </nav>
+    <article>main area</article>
+    </section>
+    <footer>footer</footer>
+    </>
   );
 }
 
