d624349b745c920d6b552da0e61ace9036993ae8
Sakhi <sakhybig@gmail.com>
2023-03-22 15:29:59 +0200

    made the categories clickable

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import './App.css';
+import Category from './components/category';
 import React,{useState} from 'react';
 
 function App() {
-  const[results,setResults]=useState([]);
+  const[categories,setCategories]=useState([]);
+  const[products,setProducts]=useState([]);
 
   React.useEffect(()=>{
     fetch("http://localhost:3001/categories")
     .then(response=>response.json())
     .then(data=>{console.log(data);
-      setResults(data);})
+      setCategories(data);})
   },[])
 
+  const handleCategoryClick=id=>{
+    fetch("http://localhost:3001/products?catID="+id)
+    .then(response=>response.json())
+    .then(data=>{console.log(data);
+      setProducts(data);})
+  }
+
+  const renderCategories=()=>{
+    return categories.map(c=>
+      <Category key={c.id} id={c.id} title={c.title} onCategoryClick={()=>handleCategoryClick(c.id)}/>);
+  }
+
+  const renserProducts=()=>{
+    return products.map(
+      p=><div>{p.title}</div>
+    )
+  }
   return (
     <>
     <header>Mini Store</header>
 
     <section>
     <nav>
-      {results.map(d => (
-        <div key={d.id}>{d.title}</div>
-      ))}
+      {categories && renderCategories()}
     </nav>
-    <article>main area</article>
+    <article><h1>Products</h1>
+      {products && renserProducts()}
+    </article>
     </section>
     <footer>footer</footer>
     </>
